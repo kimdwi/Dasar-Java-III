d384619aaba6e41b5f5982fbb186d9048d8cc0e9
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 21:16:24 +0700

    Mengganti nilai elemen String

---
+++ src/P3N_StringReplace.java
@@ -0,0 +1,10 @@
+public class P3N_StringReplace {
+    public static void main(String[] args) {
+        //Membuat Variabel
+        String nama = "Duwi";
+        nama = nama.replace("wi","yi");
+
+        //Mencetak Hasil
+        System.out.println(nama);
+    }
+}
