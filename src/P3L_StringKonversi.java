f66b7390169697e4f81cabc0be0a771be31843c6
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 21:13:37 +0700

    Mengkonversi tipe data apapun menjadi sebuah String

---
+++ src/P3L_StringKonversi.java
@@ -0,0 +1,13 @@
+public class P3L_StringKonversi {
+    public static void main(String[] args) {
+        //Konversi Tipe Data Apapun Ke String
+        double nilai = 75;
+        nilai = nilai/2;
+
+        //Menampilkan Hasil
+        System.out.println("75 / 2 = " +nilai);
+        boolean isOK = true;
+        System.out.println("" +isOK);
+        System.out.println("" +25 / 6.0);
+    }
+}
