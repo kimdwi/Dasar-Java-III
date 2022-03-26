6e8cdc578c0807b39ef6f3f73c4e1fc73ad6bda6
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 21:17:29 +0700

    Menggunakan fungsi substring

---
+++ src/P3O_StringSub.java
@@ -0,0 +1,9 @@
+public class P3O_StringSub {
+    public static void main(String[] args) {
+        //Membuat Variabel
+        String s = "Bicycle";
+        int iBegin = 1;
+        char iEnd = 3;
+        System.out.println(s.substring(iBegin,iEnd));
+    }
+}
