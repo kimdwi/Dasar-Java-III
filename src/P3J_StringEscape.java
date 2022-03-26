e98818ab1a708289441ff2c5e297b692d2a7c57f
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 21:09:39 +0700

    Menggunakan escape character pada suatu String

---
+++ src/P3J_StringEscape.java
@@ -0,0 +1,12 @@
+public class P3J_StringEscape {
+    public static void main(String[] args) {
+        //Menggunakan Escape Character
+        String str1 = "POLBENG";
+        String str2 = "polbeng";
+
+        //Menampilkan Hasil
+        System.out.println(str1+ "\n" +str2);
+        String str3 = "RPL";
+        System.out.println(str1+ "\t" +str3);
+    }
+}
