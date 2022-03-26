46b79f90768d042dfeea13e5a599190ee9b18aef
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 20:58:36 +0700

    Menggunakan fungsi absolut (abs), perpangkatan (pow), dan akar (sqrt)

---
+++ src/P3B_TestAbsPow.java
@@ -0,0 +1,17 @@
+import static java.lang.Math.abs;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class P3B_TestAbsPow {
+    public static void main(String[] args) {
+        //Membuat Variabel
+        //abs() and signum()
+        //sin(), cos(), tan(), acos(), asin(), atan()
+
+        //Menampilkan Nilai
+        System.out.println("Nilai Absolut -5 Adalah " +abs(-5));
+        System.out.println("Nilai 2 Pangkat 3 Adalah " +pow(2,3));
+        System.out.println("Nilai 8 Pangkat 1/3 Adalah " +pow(8,(1/3.0)));
+        System.out.println("Akar Kuadrat Dari 64 Adalah " +sqrt(64));
+    }
+}
