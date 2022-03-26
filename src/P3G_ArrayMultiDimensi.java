1d347e87a5843f4ba808d4ade8df6cc82daa9bba
kimdwi <parkdwiputriani@gmail.com>
2022-03-26 21:05:31 +0700

    Mendapatkan informasi panjang elemen array multi dimensi

---
+++ src/P3G_ArrayMultiDimensi.java
@@ -0,0 +1,31 @@
+public class P3G_ArrayMultiDimensi {
+    public static void main(String[] args) {
+        //Membuat Array Multidimensi
+        int x[][][][] = new int[2][][][];
+        x [0] = new int [1][][];
+        x [0][0] = new int [2][];
+        x [0][0][0] = new int [3];
+        x [0][0][1] = new int[2];
+
+        x [1] = new int [2][][];
+        x [1][0] = new int [1][];
+        x [1][0][0] = new int [2];
+        x [1][1] = new int[2][];
+        x [1][1][0] = new int [1];
+        x [1][1][1] = new int [3];
+
+        //Mencetak Array
+        System.out.println(x.length);
+        System.out.println(x[0].length);
+        System.out.println(x[0][0].length);
+        System.out.println(x[0][0][0].length);
+        System.out.println(x[0][0][1].length);
+        System.out.println();
+        System.out.println(x[1].length);
+        System.out.println(x[1][0].length);
+        System.out.println(x[1][0][0].length);
+        System.out.println(x[1][1].length);
+        System.out.println(x[1][1][0].length);
+        System.out.println(x[1][1][1].length);
+    }
+}
